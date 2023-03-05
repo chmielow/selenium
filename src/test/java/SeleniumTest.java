9ff5a7387ad5e62011a006c6f7e5f54d95111a4a
Marcin Chmielowiec <marcinchmielow@gmail.com>
2023-03-05 14:54:01 +0100

    Testing  WebDriver in Chrome, Firefox and InternetExplorer.

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,13 +1,16 @@
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.Test;
 
 public class SeleniumTest {
-    public static void main(String[] args) {
-        WebDriver driver = new FirefoxDriver();
-    }
+
     @Test
-    public void sampleTest(){
-        System.out.println("Hello!");
+    public void openGooglePage(){
+        //WebDriver driver = new ChromeDriver();
+        //WebDriver driver = new FirefoxDriver();
+        WebDriver driver = new InternetExplorerDriver();
+        driver.get("https://www.google.com");
     }
 }
