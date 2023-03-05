326f73654f114c8d4fdc427643a08f1bb7746f72
Marcin Chmielowiec <marcinchmielow@gmail.com>
2023-03-05 15:21:27 +0100

    Added method getDriver

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.InvalidArgumentException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -10,7 +11,19 @@ public class SeleniumTest {
     public void openGooglePage(){
         //WebDriver driver = new ChromeDriver();
         //WebDriver driver = new FirefoxDriver();
-        WebDriver driver = new InternetExplorerDriver();
+        WebDriver driver = getDriver("firefox");
         driver.get("https://www.google.com");
+    }
+    public WebDriver getDriver(String browser){
+        if(browser == "chrome"){
+             return new ChromeDriver();
+        } if (browser == "firefox"){
+           return new FirefoxDriver();
+        } if (browser == "ie"){
+           return new InternetExplorerDriver();
+        } else {
+            throw new InvalidArgumentException("invalid browser name");
+        }
+
     }
 }
