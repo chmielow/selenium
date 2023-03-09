feb568cc3c0630550419f93e29c395e770630791
Marcin Chmielowiec <marcinchmielow@gmail.com>
2023-03-09 14:55:33 +0100

    created class BasicActionsTest with simple example

---
+++ src/test/java/BasicActionsTest.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class BasicActionsTest {
+    @Test
+    public void performAction(){
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://testeroprogramowania.github.io/selenium/");
+
+        WebElement basicLink = driver.findElement(By.linkText("Podstawowa strona testowa"));
+        basicLink.click();
+
+        driver.findElement(By.id("fname")).click();
+
+    }
+}
