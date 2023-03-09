3a00621feee27e9ef0b5934ad1dcfc7e2225b9cc
Marcin Chmielowiec <marcinchmielow@gmail.com>
2023-03-09 16:11:27 +0100

    testing select element using select object

--- src/test/java/BasicActionsTest.java
+++ src/test/java/BasicActionsTest.java
@@ -3,7 +3,10 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class BasicActionsTest {
     @Test
@@ -24,6 +27,20 @@ public class BasicActionsTest {
 
         driver.findElement(By.cssSelector("[type='checkbox']")).click();
         driver.findElement(By.cssSelector("[value='male']")).click();
+
+        //WebElement selectCar = driver.findElement(By.cssSelector("[value='saab']"));
+        //selectCar.click();
+
+        //WebElement select = driver.findElement(By.cssSelector("select"));
+        Select cars = new Select(driver.findElement(By.cssSelector("select")));
+        cars.selectByValue("volvo");
+        List<WebElement> options = cars.getOptions();
+        for (WebElement option : options){
+            System.out.println(option.getText());
+        }
+
+
+
 
 
 
