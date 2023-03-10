0603bdfe5633b19f235b444b5d96b2bb791ccf4e
Marcin Chmielowiec <marcinchmielow@gmail.com>
2023-03-10 18:46:29 +0100

    change ChromeDriver to FirefoxDriver

--- src/test/java/IframeTest.java
+++ src/test/java/IframeTest.java
@@ -2,13 +2,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class IframeTest {
 
     @Test
     public void testIframe(){
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new FirefoxDriver();
         driver.get("https://testeroprogramowania.github.io/selenium/iframe.html");
         WebElement iframe = driver.findElement(By.cssSelector("[src='basics.html']"));
         driver.switchTo().frame(iframe);
