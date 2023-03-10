358a63463f5acb748e5fc2fd3e46cb155ad0804c
Marcin Chmielowiec <marcinchmielow@gmail.com>
2023-03-10 18:52:16 +0100

    screenshots with unique name

--- src/test/java/UploadTest.java
+++ src/test/java/UploadTest.java
@@ -1,13 +1,31 @@
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
+import java.io.File;
+import java.io.IOException;
+
 public class UploadTest {
+
     @Test
-    public void uploadFile(){
-        WebDriver driver = new ChromeDriver();
+    public void uploadFile() throws IOException {
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+        int randomNumber = (int) (Math.random() * 1000);
         driver.get("https://testeroprogramowania.github.io/selenium/fileupload.html");
+        TakesScreenshot screenshot = (TakesScreenshot) driver;
+        File before = screenshot.getScreenshotAs(OutputType.FILE);
+        String fileName = "beforeUpload" + randomNumber + ".png";
+        FileUtils.copyFile(before, new File("src/test/resources/" + fileName));
         driver.findElement(By.id("myFile")).sendKeys("C:\\Users\\Marcin\\Downloads\\Aplikacja.txt");
 
 
