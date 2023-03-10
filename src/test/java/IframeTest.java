df80019cdf418c99845a7678dd209e638cc1a6cb
Marcin Chmielowiec <marcinchmielow@gmail.com>
2023-03-10 13:39:29 +0100

    Modified IframeTest using different frame method

--- src/test/java/IframeTest.java
+++ src/test/java/IframeTest.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
@@ -9,7 +10,8 @@ public class IframeTest {
     public void testIframe(){
         WebDriver driver = new ChromeDriver();
         driver.get("https://testeroprogramowania.github.io/selenium/iframe.html");
-        driver.switchTo().frame(0);
+        WebElement iframe = driver.findElement(By.cssSelector("[src='basics.html']"));
+        driver.switchTo().frame(iframe);
         driver.findElement(By.id("fname")).sendKeys("Marcin");
         driver.switchTo().defaultContent(); //wracanie do pierwotnej strony
         System.out.println(driver.findElement(By.tagName("h1")).getText());
