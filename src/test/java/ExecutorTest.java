04f2dbdbf83e2e15f54f7bdfb404447c33d1c54f
Marcin Chmielowiec <marcinchmielow@gmail.com>
2023-03-10 13:51:14 +0100

    Created ExecutorTest to use instead click() method

---
+++ src/test/java/ExecutorTest.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class ExecutorTest {
+    @Test
+    public void executeJavascript(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://testeroprogramowania.github.io/selenium/");
+
+        WebElement basicLink = driver.findElement(By.linkText("Podstawowa strona testowa"));
+        JavascriptExecutor executor= (JavascriptExecutor) driver;
+        executor.executeScript("arguments[0].click();",basicLink);
+    }
+}
