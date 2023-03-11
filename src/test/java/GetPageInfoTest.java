c0c3d1f3dc9259387a107595bf7a2753db69c39c
Marcin Chmielowiec <marcinchmielow@gmail.com>
2023-03-11 15:53:30 +0100

    Create class GetPageInfoTest to get info from website

---
+++ src/test/java/GetPageInfoTest.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+
+public class GetPageInfoTest {
+    @Test
+    public void getInfo() {
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://testeroprogramowania.github.io/selenium/basics.html");
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+
+
+
+    }
+}
