d89c93cdc89726d54843071bce308b1614c7c54a
Marcin Chmielowiec <marcinchmielow@gmail.com>
2023-03-12 10:49:25 +0100

    Explicit wait - WebDriverWait testing

--- src/test/java/WaitTest.java
+++ src/test/java/WaitTest.java
@@ -3,10 +3,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.Assert;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
+
+import java.time.Duration;
 
 public class WaitTest {
 
@@ -17,12 +19,15 @@ public class WaitTest {
         options.addArguments("--remote-allow-origins=*");
 
         driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+       // driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://testeroprogramowania.github.io/selenium/wait2.html");
 
         WebElement button = driver.findElement(By.tagName("button"));
         button.click();
         //Thread.sleep(5000);
+        WebDriverWait wait;
+        wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("p")));
         driver.findElement(By.cssSelector("p"));
 
 
