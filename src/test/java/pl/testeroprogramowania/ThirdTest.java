9e39f12f57817b3e2daf69d78fe2e4ca2dc55b3a
Marcin Chmielowiec <marcinchmielow@gmail.com>
2023-03-12 16:39:05 +0100

    setting test priority

---
+++ src/test/java/pl/testeroprogramowania/ThirdTest.java
@@ -0,0 +1,18 @@
+package pl.testeroprogramowania;
+
+import org.testng.annotations.Test;
+
+public class ThirdTest{
+    @Test(priority = 2)
+    public void firstTest(){
+        System.out.println("First test");
+    }
+    @Test(priority = 0)
+    public void secondTest(){
+        System.out.println("Second test");
+    }
+    @Test(priority = 1)
+    public void thirdTest(){
+        System.out.println("Third test");
+    }
+}
