e25f7a29072b577ef8a2767c9f1356f95133536f
Marcin Chmielowiec <marcinchmielow@gmail.com>
2023-03-12 11:47:50 +0100

    Explicit wait - FluentWait testing

--- src/test/java/WaitTest.java
+++ src/test/java/WaitTest.java
@@ -1,9 +1,11 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
@@ -25,8 +27,10 @@ public class WaitTest {
         WebElement button = driver.findElement(By.tagName("button"));
         button.click();
         //Thread.sleep(5000);
-        WebDriverWait wait;
-        wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+        FluentWait<WebDriver> wait = new FluentWait<>(driver);
+        wait.ignoring(NoSuchElementException.class);
+        wait.withTimeout(Duration.ofSeconds(10));
+        wait.pollingEvery(Duration.ofSeconds(1)); //interwał
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("p")));
         driver.findElement(By.cssSelector("p"));
 
