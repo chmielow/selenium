08f852dfaa3a94984c0bfdb225b36345c9e97c76
Marcin Chmielowiec <marcinchmielow@gmail.com>
2023-03-12 16:13:34 +0100

    @AfterTest and @AfterMethod

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,13 +1,25 @@
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 
 public class BaseTest {
-@BeforeTest
-    public void beforeTest(){
+    @BeforeTest
+    public void beforeTest() {
         System.out.println("I am running before test");
     }
-@BeforeMethod
-    public void beforeMethod(){
+
+    @BeforeMethod
+    public void beforeMethod() {
         System.out.println("I am running before method");
+    }
+
+    @AfterTest
+    public void afterTest() {
+        System.out.println("I am running after test");
+    }
+   @AfterMethod
+    public void afterMethod() {
+        System.out.println("I am running after method");
     }
 }
