756885ad01637e3b6f2e8acc77e04e0d75c31a59
Marcin Chmielowiec <marcinchmielow@gmail.com>
2023-03-12 16:50:49 +0100

    dependsOnMethod

---
+++ src/test/java/pl/testeroprogramowania/FourthTest.java
@@ -0,0 +1,18 @@
+package pl.testeroprogramowania;
+
+import org.testng.annotations.Test;
+
+public class FourthTest {
+    @Test(dependsOnMethods ={"thirdTest"} )
+    public void firstTest(){
+        System.out.println("First test");
+    }
+    @Test(dependsOnMethods ={"firstTest"} )
+    public void secondTest(){
+        System.out.println("Second test");
+    }
+    @Test
+    public void thirdTest(){
+        System.out.println("Third test");
+    }
+}
